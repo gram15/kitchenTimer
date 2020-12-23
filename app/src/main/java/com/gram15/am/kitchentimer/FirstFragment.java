abe42632fae0b13b9354539784e8af5695ba26ad
amantovan <amantovan@outlook.com>
2020-12-23 23:01:40 +0100

    basic main activity layout

--- app/src/main/java/com/gram15/am/kitchentimer/FirstFragment.java
+++ app/src/main/java/com/gram15/am/kitchentimer/FirstFragment.java
@@ -1,15 +1,34 @@
 package com.gram15.am.kitchentimer;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+import java.util.concurrent.TimeUnit;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 public class FirstFragment extends Fragment {
+
+    private long timeCountInMilliSeconds = 1 * 60000;
+
+    private TimerStatus timerStatus = TimerStatus.STOPPED;
+    private ProgressBar progressBarCircle;
+    private EditText editTextMinute;
+    private TextView textViewTime;
+    private MaterialButton btnViewStartStop;
+    private CountDownTimer countDownTimer;
 
     @Override
     public View onCreateView(
@@ -23,12 +42,143 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
+        progressBarCircle = (ProgressBar) view.findViewById(R.id.progressBarCircle);
+        editTextMinute = (EditText) view.findViewById(R.id.editTextMinute);
+        textViewTime = (TextView) view.findViewById(R.id.textViewTime);
+        btnViewStartStop = (MaterialButton) view.findViewById(R.id.btnViewStartStop);
+        btnViewStartStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startAndStop();
+            }
+        });
+
+
+
+        /*view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 NavHostFragment.findNavController(FirstFragment.this)
                         .navigate(R.id.action_FirstFragment_to_SecondFragment);
             }
         });
+        */
+
+    }
+
+
+    private void startAndStop() {
+        if (timerStatus == TimerStatus.STOPPED) {
+
+            // call to initialize the timer values
+            setTimerValues();
+            // call to initialize the progress bar values
+            setProgressBarValues();
+            // changing play icon to stop icon
+            // FIXME AM
+            // btnViewStartStop.setIcon(android.dr;
+            // making edit text not editable
+            editTextMinute.setEnabled(false);
+            // changing the timer status to started
+            timerStatus = TimerStatus.STARTED;
+            // call to start the count down timer
+            startCountDownTimer();
+
+        } else {
+            // changing stop icon to start icon
+            // imageViewStartStop.setImageResource(R.drawable.icon_start);
+            // making edit text editable
+            editTextMinute.setEnabled(true);
+            // changing the timer status to stopped
+            timerStatus = TimerStatus.STOPPED;
+            stopCountDownTimer();
+
+        }
+    }
+
+    /**
+     * method to initialize the values for count down timer
+     */
+    private void setTimerValues() {
+        int time = 0;
+        if (!editTextMinute.getText().toString().isEmpty()) {
+            // fetching value from edit text and type cast to integer
+            time = Integer.parseInt(editTextMinute.getText().toString().trim());
+        } else {
+            // toast message to fill edit text
+            Toast.makeText(getContext(), getString(R.string.message_minutes), Toast.LENGTH_LONG).show();
+        }
+        // assigning values after converting to milliseconds
+        timeCountInMilliSeconds = time * 60 * 1000;
+    }
+
+    /**
+     * method to start count down timer
+     */
+    private void startCountDownTimer() {
+
+        countDownTimer = new CountDownTimer(timeCountInMilliSeconds, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+                textViewTime.setText(hmsTimeFormatter(millisUntilFinished));
+
+                progressBarCircle.setProgress((int) (millisUntilFinished / 1000));
+
+            }
+
+            @Override
+            public void onFinish() {
+
+                textViewTime.setText(hmsTimeFormatter(timeCountInMilliSeconds));
+                // call to initialize the progress bar values
+                setProgressBarValues();
+
+                // changing stop icon to start icon
+                //         imageViewStartStop.setImageResource(R.drawable.icon_start);
+                // making edit text editable
+                editTextMinute.setEnabled(true);
+                // changing the timer status to stopped
+                timerStatus = TimerStatus.STOPPED;
+            }
+
+        }.start();
+        countDownTimer.start();
+    }
+
+    /**
+     * method to stop count down timer
+     */
+    private void stopCountDownTimer() {
+        countDownTimer.cancel();
+    }
+
+    /**
+     * method to set circular progress bar values
+     */
+    private void setProgressBarValues() {
+
+        progressBarCircle.setMax((int) timeCountInMilliSeconds / 1000);
+        progressBarCircle.setProgress((int) timeCountInMilliSeconds / 1000);
+    }
+
+
+    /**
+     * method to convert millisecond to time format
+     *
+     * @param milliSeconds
+     * @return HH:mm:ss time formatted string
+     */
+    private String hmsTimeFormatter(long milliSeconds) {
+
+        String hms = String.format("%02d:%02d:%02d",
+                TimeUnit.MILLISECONDS.toHours(milliSeconds),
+                TimeUnit.MILLISECONDS.toMinutes(milliSeconds) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(milliSeconds)),
+                TimeUnit.MILLISECONDS.toSeconds(milliSeconds) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(milliSeconds)));
+
+        return hms;
+
+
     }
 }
---
+++ app/src/main/java/com/gram15/am/kitchentimer/TimerStatus.java
@@ -0,0 +1,9 @@
+package com.gram15.am.kitchentimer;
+
+/**
+ * Created by AMantovan on 23/12/2020.
+ */
+public enum TimerStatus {
+    STARTED,
+    STOPPED
+}
